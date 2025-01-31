ea2238976af939c8127af6d7f2bea7e93464730c
Ivan <steoanchikov2017@ya.ru>
2025-01-31 21:28:43 +0300

    test

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,12 @@ const router = createRouter({
 	routes: [
 		{
 			path: '/',
-			component: import('/src/views/index.vue'),
+			component: import('@/src/views/index.vue'),
 		},
 		{
 			path: '/contact',
 			name: 'contact',
-			component: import('/src/views/contact.vue'),
+			component: import('@/src/views/contact.vue'),
 		},
 	],
 })
