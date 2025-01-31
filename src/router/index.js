c7710c7cd3c8a7402a47458d39adecd78a8a1a77
Ivan <steoanchikov2017@ya.ru>
2025-01-31 21:46:47 +0300

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import IndexView from '@/views/index.vue'
 
 const router = createRouter({
 	history: createWebHistory(),
 	routes: [
 		{
 			path: '/',
-			component: import('@/views/index.vue'),
+			component: () => import('@/views/index.vue'),
 		},
 		{
 			path: '/contact',
 			name: 'contact',
-			component: import('@/views/contact.vue'),
+			component: () => import('@/views/contact.vue'),
 		},
 	],
 })
